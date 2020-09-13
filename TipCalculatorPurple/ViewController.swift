1536bc761fcd124d2384d2adbe1e5114208a25ab
Liberty <libisin@gmail.com>
2020-09-12 21:57:37 -0700

    Initial Commit

---
+++ TipCalculatorPurple/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipCalculatorPurple
+//
+//  Created by Liberty Locsin on 9/12/20.
+//  Copyright © 2020 Liberty Locsin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
